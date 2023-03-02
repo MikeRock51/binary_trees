03923b5743bb2622c4e293d6bd33c91c424dba3c
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-02 15:53:19 +0100

    Add task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,4 +7,17 @@
  * Return: 0 if pointer is full
  * Or 1 otherwise
  */
-int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+
+	return (0);
+}
