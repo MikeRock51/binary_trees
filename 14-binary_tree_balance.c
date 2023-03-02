6fcc05a24fd4a2155498bbb7924c02d17352a941
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-02 14:15:30 +0100

    Bettify 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,31 +18,31 @@ int max(int left, int right)
 /**
  * tree_height - Calculates the height of a tree
  * @tree: A pointer to the root node of the tree to calculate
- * 
+ *
  * Return: The height of the tree.
-*/
+ */
 int tree_height(binary_tree_t *tree)
 {
-    int right_height = 0, left_height = 0;
+	int right_height = 0, left_height = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 	else if (tree->left || tree->right)
 		return ((tree_height(tree->left) + (tree_height(tree->right))));
 
 	left_height = tree_height(tree->left);
 	right_height = tree_height(tree->right);
 
-	return (max(left_height, right_height) +1);
+	return (max(left_height, right_height) + 1);
 }
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: A pointer to the root node of the tree to measure
- * 
+ *
  * Return: The balance factor of the tree
  * Or 0, if tree is NULL
-*/
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height = 0, right_height = 0;
@@ -53,5 +53,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	left_height = tree_height(tree->left);
 	right_height = tree_height(tree->right);
 
-	return(left_height - right_height);
+	return (left_height - right_height);
 }
