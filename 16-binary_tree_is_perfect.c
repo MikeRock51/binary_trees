f6e519b945e9185de832288978a767cc7e55f078
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-06 06:23:23 +0100

    Update 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,21 +21,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 }
 
 /**
- * max - Returns the largest of two numbers
- * @left: The first number
- * @right: The second number
- *
- * Return: The max number
- */
-size_t max(size_t left, size_t right)
-{
-	if (left >= right)
-		return (left);
-	else
-		return (right);
-}
-
-/**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: A pointer to the root node of the tree to measure
  *
@@ -43,19 +28,16 @@ size_t max(size_t left, size_t right)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t l = 0, r = 0;
 
-	if (!tree)
-		return (0);
+	if (tree)
+	{
+		l = (tree->left) ? 1 + binary_tree_height(tree->left) : 1;
 
-	left_height = binary_tree_height(tree->left);
-	if (tree->left)
-		left_height += 1;
-	right_height = binary_tree_height(tree->right);
-	if (tree->right)
-		right_height += 1;
-
-	return (max(left_height, right_height));
+		r = (tree->right) ? 1 + binary_tree_height(tree->right) : 1;
+		return (l > r ? l : r);
+	}
+	return (0);
 }
 
 /**
